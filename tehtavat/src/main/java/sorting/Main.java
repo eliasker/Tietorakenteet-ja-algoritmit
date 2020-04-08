ff09b95678b2463fb20b1935de91a6675c0de342
eliasker <eliasker@gmail.com>
2020-04-08 19:20:56 +0300

    3) Variable for tracking comparisons

--- tehtavat/src/main/java/sorting/Main.java
+++ tehtavat/src/main/java/sorting/Main.java
@@ -4,14 +4,15 @@ import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
-  final static int SAMPLE_SIZE = 50000;
-  final static int USE_ALGORITHM = 0; // 0 is select sort, 1 is merge and 2 is quick
+  final static int SAMPLE_SIZE = 10000;
+  final static int USE_ALGORITHM = 1; // 0 is select sort, 1 is merge and 2 is quick
   final static String[] algorithmNames = { "Select sort", "Merge sort", "Quick Sort" };
+  public static int comparisons = 0;
 
   public static void main(String[] args) {
     int[] numbers = generateSample();
     System.out.println("Initial array generated");
-
+    // printList(numbers);
     long startTime = 0;
     long elapsedTime = 0;
     switch (USE_ALGORITHM) {
@@ -31,9 +32,14 @@ public class Main {
         elapsedTime = System.nanoTime() - startTime;
         break;
     }
+    System.out.println("\n");
+    // printList(numbers);
+
     System.out.println("Sorting done.");
+    System.out.println("Sample size: " + SAMPLE_SIZE);
     System.out.println("Used algorithm: " + algorithmNames[USE_ALGORITHM]);
     System.out.println("It took: " + TimeUnit.MILLISECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS) + "ms");
+    System.out.println("Comparisons done: " + comparisons);
   }
 
   public static int[] generateSample() {
@@ -59,10 +65,12 @@ public class Main {
     for (i = 0; i < SAMPLE_SIZE; i++) {
       smallest = i;
       for (j = i + 1; j < SAMPLE_SIZE; j++) {
+        comparisons++;
         if (numbers[j] < numbers[smallest]) {
           smallest = j;
         }
       }
+      comparisons++;
       if (smallest != i) {
         temp = numbers[smallest];
         numbers[smallest] = numbers[i];
@@ -72,8 +80,10 @@ public class Main {
   }
 
   public static void mergeSort(int[] numbers, int low, int high) {
-    if (high <= low)
+    if (high <= low) {
+      comparisons++;
       return;
+    }
 
     int mid = (low + high) / 2;
     mergeSort(numbers, low, mid);
@@ -85,16 +95,20 @@ public class Main {
     int leftArray[] = new int[mid - low + 1];
     int rightArray[] = new int[high - mid];
 
-    for (int i = 0; i < leftArray.length; i++)
+    for (int i = 0; i < leftArray.length; i++) {
       leftArray[i] = numbers[low + i];
-    for (int i = 0; i < rightArray.length; i++)
+    }
+    for (int i = 0; i < rightArray.length; i++) {
       rightArray[i] = numbers[mid + i + 1];
+    }
 
     int leftIndex = 0;
     int rightIndex = 0;
 
     for (int i = low; i < high + 1; i++) {
+      comparisons++;
       if (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+        comparisons++;
         if (leftArray[leftIndex] < rightArray[rightIndex]) {
           numbers[i] = leftArray[leftIndex];
           leftIndex++;
@@ -103,9 +117,11 @@ public class Main {
           rightIndex++;
         }
       } else if (leftIndex < leftArray.length) {
+        comparisons++;
         numbers[i] = leftArray[leftIndex];
         leftIndex++;
       } else if (rightIndex < rightArray.length) {
+        comparisons++;
         numbers[i] = rightArray[rightIndex];
         rightIndex++;
       }
