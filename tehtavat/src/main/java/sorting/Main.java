ae030292eda129ee5547be0eb6aab65bd180620b
eliasker <eliasker@gmail.com>
2020-04-08 15:11:21 +0300

    3) Merge sort

--- tehtavat/src/main/java/sorting/Main.java
+++ tehtavat/src/main/java/sorting/Main.java
@@ -3,16 +3,20 @@ package sorting;
 import java.util.Random;
 
 public class Main {
-  final static int SAMPLE_SIZE = 50;
+  final static int SAMPLE_SIZE = 1000;
 
   public static void main(String[] args) {
     int[] numbers = generateSample();
+    System.out.println("Initial array");
+    //printList(numbers);
+    //System.out.println("\n\n");
 
-    printList(numbers);
+    //System.out.println("Select sort");
+    //selectSort(numbers);
 
-    System.out.println("\n\n");
-    int[] sortedNumbers = selectSort(numbers);
-    printList(sortedNumbers);
+    System.out.println("Merge sort");
+    mergeSort(numbers, 0, numbers.length - 1);
+    System.out.println("Done");
   }
 
   public static int[] generateSample() {
@@ -32,26 +36,62 @@ public class Main {
     }
   }
 
-  public static int[] selectSort(int[] numbers) {
-    int[] sortedNumbers = new int[SAMPLE_SIZE];
-    int i, j, k, temp, smallest;
-    for (k = 0; k < SAMPLE_SIZE; k++)
-      sortedNumbers[k] = numbers[k];
+  public static void selectSort(int[] numbers) {
+    int i, j, temp, smallest;
 
-    int[] sorted = numbers;
     for (i = 0; i < SAMPLE_SIZE; i++) {
       smallest = i;
       for (j = i + 1; j < SAMPLE_SIZE; j++) {
-        if (sorted[j] < sorted[smallest]) {
+        if (numbers[j] < numbers[smallest]) {
           smallest = j;
         }
       }
       if (smallest != i) {
-        temp = sorted[smallest];
-        sorted[smallest] = sorted[i];
-        sorted[i] = temp;
+        temp = numbers[smallest];
+        numbers[smallest] = numbers[i];
+        numbers[i] = temp;
       }
     }
-    return sorted;
+  }
+
+  public static void mergeSort(int[] numbers, int low, int high) {
+    if (high <= low)
+      return;
+
+    int mid = (low + high) / 2;
+    mergeSort(numbers, low, mid);
+    mergeSort(numbers, mid + 1, high);
+    merge(numbers, low, mid, high);
+  }
+
+  public static void merge(int[] numbers, int low, int mid, int high) {
+    int leftArray[] = new int[mid - low + 1];
+    int rightArray[] = new int[high - mid];
+
+    for (int i = 0; i < leftArray.length; i++)
+      leftArray[i] = numbers[low + i];
+    for (int i = 0; i < rightArray.length; i++)
+      rightArray[i] = numbers[mid + i + 1];
+
+    int leftIndex = 0;
+    int rightIndex = 0;
+
+    for (int i = low; i < high + 1; i++) {
+      if (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+        if (leftArray[leftIndex] < rightArray[rightIndex]) {
+          numbers[i] = leftArray[leftIndex];
+          leftIndex++;
+        } else {
+          numbers[i] = rightArray[rightIndex];
+          rightIndex++;
+        }
+      } else if (leftIndex < leftArray.length) {
+        numbers[i] = leftArray[leftIndex];
+        leftIndex++;
+      } else if (rightIndex < rightArray.length) {
+        numbers[i] = rightArray[rightIndex];
+        rightIndex++;
+      }
+    }
   }
 }
