8155bc4e0ff534c77123a1154088a3ebaea0c201
eliasker <eliasker@gmail.com>
2020-04-08 14:18:19 +0300

    3) Sorting array with select sort

--- tehtavat/src/main/java/sorting/Main.java
+++ tehtavat/src/main/java/sorting/Main.java
@@ -3,10 +3,16 @@ package sorting;
 import java.util.Random;
 
 public class Main {
-  final static int SAMPLE_SIZE = 30000;
+  final static int SAMPLE_SIZE = 50;
 
   public static void main(String[] args) {
     int[] numbers = generateSample();
+
+    printList(numbers);
+
+    System.out.println("\n\n");
+    int[] sortedNumbers = selectSort(numbers);
+    printList(sortedNumbers);
   }
 
   public static int[] generateSample() {
@@ -14,16 +20,38 @@ public class Main {
     Random r = new Random();
     for (int i = 0; i < SAMPLE_SIZE; i++) {
       numbers[i] = r.nextInt(1000);
+    }
+    return numbers;
+  }
+
+  public static void printList(int[] numbers) {
+    for (int i = 0; i < numbers.length; i++) {
       System.out.print(numbers[i] + " ");
       if (i > 0 && i % 20 == 0)
         System.out.println();
     }
-
-    return numbers;
   }
 
-  // TODO: implement
-  public static void selectSort() {
+  public static int[] selectSort(int[] numbers) {
+    int[] sortedNumbers = new int[SAMPLE_SIZE];
+    int i, j, k, temp, smallest;
+    for (k = 0; k < SAMPLE_SIZE; k++)
+      sortedNumbers[k] = numbers[k];
 
+    int[] sorted = numbers;
+    for (i = 0; i < SAMPLE_SIZE; i++) {
+      smallest = i;
+      for (j = i + 1; j < SAMPLE_SIZE; j++) {
+        if (sorted[j] < sorted[smallest]) {
+          smallest = j;
+        }
+      }
+      if (smallest != i) {
+        temp = sorted[smallest];
+        sorted[smallest] = sorted[i];
+        sorted[i] = temp;
+      }
+    }
+    return sorted;
   }
 }
